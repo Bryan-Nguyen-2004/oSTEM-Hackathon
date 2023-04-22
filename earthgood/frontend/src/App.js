d6e743f6463983a499ff307fdf6944c1735bbe5c
Kevin Yuan <kev.yuan01@gmail.com>
2023-04-22 15:08:43 -0700

    It worked lol

--- earthgood/frontend/src/App.js
+++ earthgood/frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+const box1 = document.querySelector('.box1')
+const thevideo = box1.querySelector('video')
 
-function App() {
+
+
+function App() {  
   return (
     <div className="App">
 
     </div>
   );
 }
+
 
 export default App;
